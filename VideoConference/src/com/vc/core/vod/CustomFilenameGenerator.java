8875bfc1f68b07914ce9bbe765cde46eacfce64a
pengliu1980 <pengliu1980@23bfff96-ef8e-11de-91ba-91c70a792394>
2010-02-12 03:00:13 +0000

    Add hadoop dfs support

--- VideoConference/src/com/vc/core/vod/CustomFilenameGenerator.java
+++ VideoConference/src/com/vc/core/vod/CustomFilenameGenerator.java
@@ -1,5 +1,15 @@
 package com.vc.core.vod;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.red5.logging.Red5LoggerFactory;
 import org.red5.server.api.IScope;
 import org.red5.server.api.stream.IStreamFilenameGenerator;
@@ -67,4 +77,65 @@ public class CustomFilenameGenerator implements IStreamFilenameGenerator {
 		return true;
 	}
 
+	public static void main(String[] a) {
+		Configuration conf = new Configuration();
+
+		// conf.set("hadoop.job.ugi", "root,123"); // 设置存储服务器的用户名，密码
+
+		String uri = "hdfs://172.0.2.193:54310/";
+
+		// try {
+		// String str = "Hello world";
+		//
+		// FileSystem fs = FileSystem.get(URI.create(uri), conf);
+		//			
+		// fs.deleteOnExit(new Path("/helloworld"));
+		//			
+		// // java.io.OutputStream out = fs.create(new Path("/helloworld"));
+		// //
+		// // out.write(str.getBytes());
+		// //
+		// // out.flush();
+		// //
+		// // out.close();
+		// } catch (IOException e) {
+		//
+		// e.printStackTrace();
+		// }
+
+		InputStream in = null;
+		OutputStream out = null;
+
+		// FSDataInputStream in = null;
+		try {
+
+			File file = new File("/home/ammen/Test/videoStreams/terminator.flv");
+			in = new FileInputStream(file);
+
+			FileSystem fs = FileSystem.get(URI.create(uri), conf);
+			out = fs.create(new Path("/vod/videoStreams/terminator.flv"));
+
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {}
+			}
+
+		}
+	}
 }
