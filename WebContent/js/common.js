dc83820d8c4ceaffa332dfcd59c2de7d655723d5
pengliu1980 <pengliu1980@23bfff96-ef8e-11de-91ba-91c70a792394>
2010-04-20 07:51:00 +0000

    Implement ajax login with spring security

--- WebContent/js/common.js
+++ WebContent/js/common.js
@@ -1,50 +1,53 @@
-var playListIDs="";
+var playListIDs = "";
 
-$(document).ready(function(){
-	if ($("#lzappContainer").length > 0){
-		$("#contentpanel").css("width","100%");
+$(document).ready(function() {
+	if ($("#lzappContainer").length > 0) {
+		$("#contentpanel").css("width", "100%");
 	}
 });
 function flushValidateCode() {
 	// set timeout is workaroud for update captcha in IE
 	setTimeout(function() {
-	$('#ccode').attr('src','signUp/captcha?_=' + parseInt(Math.random() * 1000 + 1));}, 100);
+		$('#ccode').attr('src',
+				'signUp/captcha?_=' + parseInt(Math.random() * 1000 + 1));
+	}, 100);
 }
 
-function vidContents(id){
-	
-	
-	if (playListIDs.indexOf(id)==-1){
-		playListIDs = playListIDs+","+id;
-		var vidContent = '#vidContent'+id;
-		$('#vid'+id).bt({
-			trigger: ['focus mouseover', 'blur'],
-			contentSelector: "$('"+vidContent+"')",
-			positions: ['right', 'left'],
-			clickAnywhereToClose: true,              
-		    closeWhenOthersOpen: true, 
-		    shrinkToFit: false,
-			fill: '#F4F4F4',
-			strokeStyle: '#666666', 
-			spikeLength: 20,
-			spikeGirth: 10,
-			width: 350,
-			overlap: 0,
-			centerPointY: 1,
-			cornerRadius: 0, 
-			cssStyles: {
-				fontFamily: '"Lucida Grande",Helvetica,Arial,Verdana,sans-serif', 
-				fontSize: '12px',
-				padding: '10px 14px'
-			},
+function vidContents(id) {
 
-			shadow: true,
-			shadowColor: 'rgba(0,0,0,.5)',
-			shadowBlur: 8,
-			shadowOffsetX: 4,
-			shadowOffsetY: 4
-		});
-	}else{
-		//alert("不加载");
+	if (playListIDs.indexOf(id) == -1) {
+		playListIDs = playListIDs + "," + id;
+		var vidContent = '#vidContent' + id;
+		$('#vid' + id)
+				.bt(
+						{
+							trigger : [ 'focus mouseover', 'blur' ],
+							contentSelector : "$('" + vidContent + "')",
+							positions : [ 'right', 'left' ],
+							clickAnywhereToClose : true,
+							closeWhenOthersOpen : true,
+							shrinkToFit : false,
+							fill : '#F4F4F4',
+							strokeStyle : '#666666',
+							spikeLength : 20,
+							spikeGirth : 10,
+							width : 350,
+							overlap : 0,
+							centerPointY : 1,
+							cornerRadius : 0,
+							cssStyles : {
+								fontFamily : '"Lucida Grande",Helvetica,Arial,Verdana,sans-serif',
+								fontSize : '12px',
+								padding : '10px 14px'
+							},
+
+							shadow : true,
+							shadowColor : 'rgba(0,0,0,.5)',
+							shadowBlur : 8,
+							shadowOffsetX : 4,
+							shadowOffsetY : 4
+						});
+	} else {
+		// alert("不加载");
 	}
 }
